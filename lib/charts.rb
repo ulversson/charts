d89309e850582976ac4f1a6695af67e714f76fef
ulverson <marcin.walczak@gmail.com>
2017-06-09 18:13:36 +0200

    Fix file loading error

--- lib/charts.rb
+++ lib/charts.rb
@@ -2,9 +2,9 @@ module Charts
 end  
 require_relative "charts/config"
 require_relative "charts/api_request"
+require_relative "charts/cleaner"
 require_relative "charts/response_storage"
 require_relative "charts/types/chart"
 require_relative "charts/types/redis_chart"
 require_relative "charts/types/non_redis_chart"
-require_relative "charts/cleaner"
 require_relative "charts/response_processors/nasdaq_response_processor"
